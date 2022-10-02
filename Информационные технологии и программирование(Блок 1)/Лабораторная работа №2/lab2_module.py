517ff1ffc8a1520b2c84a197988d5270153919c9
aprolad <aprolad@gmail.com>
2022-10-02 20:13:33 +0300

    code done

---
+++ Информационные технологии и программирование(Блок 1)/Лабораторная работа №2/lab2_module.py
@@ -0,0 +1,27 @@
+import math
+import logging
+
+def calc(x, y, a, b):
+    if -4 < x <= 1 and y > -2:
+        min = x ** y
+        if min > math.e ** x:
+            min = math.e ** x
+        if min > a:
+            min = a
+        result = math.tan(x + y*x + a ** 2 * min) ** 2
+    elif 1 < x <= 5 and -2 <= y < 8:
+        max = x + a ** 2
+        if max < y:
+            max = y
+        min = y * math.sin(x)
+        if min > a:
+            min = a
+        try:
+            result = max / min
+        except Exception as e:
+            print("Деление на ноль!")
+            logging.error(str(e))
+            exit()
+    else:
+        result = b + math.sin(math.fabs(math.e ** x)) ** 2
+    return result
--- Информационные технологии и программирование(Блок 1)/Лабораторная работа №2/main.py
+++ Информационные технологии и программирование(Блок 1)/Лабораторная работа №2/main.py
@@ -1,23 +1,17 @@
 import math
-def calc(x, y):
-    result = (1 - math.e**(x*y))/(0.7 * math.log10(math.fabs(1 - x**2)))
-    return result
+import logging
+from lab2_module import *
+
+
+logging.basicConfig(filename="log.txt", level=logging.DEBUG)
 x = float(input("Введите x "))
 y = float(input("Введите y "))
-result = calc(x, y)
-c = math.ceil(result)
-t = math.trunc(result)
-f = math.floor(result)
-r = round(result, 3)
-print("Результат работы программы: " + str(result))
-print("Округленный до большего результат: " + str(c))
-print("Усеченный до целого результат: " + str(t))
-print("Округленный до меньшего результат: " + str(f))
-print("Округленный с точностью до трех знаков результат: " + str(r))
-f = open("lab1result.txt", "w")
-f.write("Результат работы программы: " + str(result) + "\n")
-f.write("Округленный до большего результат: " + str(math.ceil(result)))
-f.write("\nУсеченный до целого результат: " + str(math.trunc(result)))
-f.write("\nОкругленный до меньшего результат: " + str(math.floor(result)))
-f.write("\nОкругленный с точностью до трех знаков результат: " + str(round(result, 3)))
-f.close()
+a = float(input("Введите a "))
+b = float(input("Введите b "))
+t = calc(x, y, a, b)
+print("Результат программы: " + str(t))
+try:
+    with open("lab2result.txt", "w") as f:
+        f.write("Результат работы программы: " + str(t) + "\n")
+except Exception as e:
+    logging.error(str(e))
